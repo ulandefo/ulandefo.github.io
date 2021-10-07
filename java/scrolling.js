3db070401ed48bddb24e0089bf1c5a7d9e5d7400
ulandefo <92108666+ulandefo@users.noreply.github.com>
2021-10-07 18:31:52 +0300

    Add files via upload

---
+++ java/scrolling.js
@@ -0,0 +1,8 @@
+const back2Top = document.querySelector('#back2Top');
+
+
+
+back2Top.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({top:0, left:0, behavior: 'smooth'});
+});
